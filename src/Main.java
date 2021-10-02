f8e1bc5a8bcacb237c6d2473c9bb98e5673fd238
AlinaNabi <amnabieva@gmail.com>
2021-10-02 04:44:48 +0300

    adding code

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+       int ticketPrice = 2000;
+       byte bonusMile = 20;
+       int mile;
+
+       mile = ticketPrice/bonusMile;
+        System.out.println(mile);
+
+}
+}
+
